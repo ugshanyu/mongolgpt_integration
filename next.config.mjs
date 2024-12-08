0d4531002bbc2f8ab38063b0b5964b73dbe5bfd3
Uuganbayar <88976978+ugshanyu@users.noreply.github.com>
2024-12-08 07:22:47 -0700

    o

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  poweredByHeader: false,
-  compress: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
+module.exports = {
+  assetPrefix: 'https://logic.mongolai.mn',
+  // Add this if you're behind a proxy
+  basePath: '/front-end',
 }
-
-module.exports = nextConfig
