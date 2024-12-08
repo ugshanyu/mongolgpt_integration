e04fcb085271e2cdf36ce2e1e9b2c6ce0280329f
Uuganbayar <88976978+ugshanyu@users.noreply.github.com>
2024-12-08 20:49:47 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/front-end',
-  assetPrefix: 'https://logic.mongolai.mn/front-end'
+  assetPrefix: 'https://logic.mongolai.mn/front-end',
+  // Add this to ensure all resources use HTTPS
+  images: {
+    domains: ['logic.mongolai.mn'],
+    protocol: 'https'
+  }
 }
-
 module.exports = nextConfig
